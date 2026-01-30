6b4be9b3e6579239049d55ed8add07940f5e4f16
KTibow <KTibow@users.noreply.github.com>
2026-01-29 19:14:11 -0800

    issue persists

--- dist/main.js
+++ dist/main.js
@@ -1,42 +1,50 @@
-import { __commonJS, __toESM } from "./chunk-DbKvDyjX.js";
+import { r as __toESM, t as __commonJSMin } from "./chunk-CaR5F9JI.js";
 
-//#region node_modules/.pnpm/set-cookie-parser@2.7.1/node_modules/set-cookie-parser/lib/set-cookie.js
-var require_set_cookie = /* @__PURE__ */ __commonJS({ "node_modules/.pnpm/set-cookie-parser@2.7.1/node_modules/set-cookie-parser/lib/set-cookie.js": ((exports, module) => {
+//#region node_modules/.pnpm/set-cookie-parser@2.7.2/node_modules/set-cookie-parser/lib/set-cookie.js
+var require_set_cookie = /* @__PURE__ */ __commonJSMin(((exports, module) => {
 	var defaultParseOptions = {
 		decodeValues: true,
 		map: false,
 		silent: false
 	};
+	function isForbiddenKey(key) {
+		return typeof key !== "string" || key in {};
+	}
+	function createNullObj() {
+		return Object.create(null);
+	}
 	function isNonEmptyString(str) {
 		return typeof str === "string" && !!str.trim();
 	}
 	function parseString(setCookieValue, options) {
 		var parts = setCookieValue.split(";").filter(isNonEmptyString);
-		var nameValuePairStr = parts.shift();
-		var parsed = parseNameValuePair(nameValuePairStr);
+		var parsed = parseNameValuePair(parts.shift());
 		var name = parsed.name;
 		var value = parsed.value;
 		options = options ? Object.assign({}, defaultParseOptions, options) : defaultParseOptions;
+		if (isForbiddenKey(name)) return null;
 		try {
 			value = options.decodeValues ? decodeURIComponent(value) : value;
 		} catch (e) {
-			console.error("set-cookie-parser encountered an error while decoding a cookie with value '" + value + "'. Set options.decodeValues to false to disable this feature.", e);
+			console.error("set-cookie-parser: failed to decode cookie value. Set options.decodeValues=false to disable decoding.", e);
 		}
-		var cookie = {
-			name,
-			value
-		};
+		var cookie = createNullObj();
+		cookie.name = name;
+		cookie.value = value;
 		parts.forEach(function(part) {
 			var sides = part.split("=");
 			var key = sides.shift().trimLeft().toLowerCase();
-			var value$1 = sides.join("=");
-			if (key === "expires") cookie.expires = new Date(value$1);
-			else if (key === "max-age") cookie.maxAge = parseInt(value$1, 10);
-			else if (key === "secure") cookie.secure = true;
+			if (isForbiddenKey(key)) return;
+			var value = sides.join("=");
+			if (key === "expires") cookie.expires = new Date(value);
+			else if (key === "max-age") {
+				var n = parseInt(value, 10);
+				if (!Number.isNaN(n)) cookie.maxAge = n;
+			} else if (key === "secure") cookie.secure = true;
 			else if (key === "httponly") cookie.httpOnly = true;
-			else if (key === "samesite") cookie.sameSite = value$1;
+			else if (key === "samesite") cookie.sameSite = value;
 			else if (key === "partitioned") cookie.partitioned = true;
-			else cookie[key] = value$1;
+			else if (key) cookie[key] = value;
 		});
 		return cookie;
 	}
@@ -53,10 +61,10 @@ var require_set_cookie = /* @__PURE__ */ __commonJS({ "node_modules/.pnpm/set-co
 			value
 		};
 	}
-	function parse$1(input, options) {
+	function parse(input, options) {
 		options = options ? Object.assign({}, defaultParseOptions, options) : defaultParseOptions;
 		if (!input) if (!options.map) return [];
-		else return {};
+		else return createNullObj();
 		if (input.headers) if (typeof input.headers.getSetCookie === "function") input = input.headers.getSetCookie();
 		else if (input.headers["set-cookie"]) input = input.headers["set-cookie"];
 		else {
@@ -69,12 +77,15 @@ var require_set_cookie = /* @__PURE__ */ __commonJS({ "node_modules/.pnpm/set-co
 		if (!Array.isArray(input)) input = [input];
 		if (!options.map) return input.filter(isNonEmptyString).map(function(str) {
 			return parseString(str, options);
-		});
-		else return input.filter(isNonEmptyString).reduce(function(cookies, str) {
-			var cookie = parseString(str, options);
-			cookies[cookie.name] = cookie;
-			return cookies;
-		}, {});
+		}).filter(Boolean);
+		else {
+			var cookies = createNullObj();
+			return input.filter(isNonEmptyString).reduce(function(cookies, str) {
+				var cookie = parseString(str, options);
+				if (cookie && !isForbiddenKey(cookie.name)) cookies[cookie.name] = cookie;
+				return cookies;
+			}, cookies);
+		}
 	}
 	function splitCookiesString(cookiesString) {
 		if (Array.isArray(cookiesString)) return cookiesString;
@@ -117,11 +128,11 @@ var require_set_cookie = /* @__PURE__ */ __commonJS({ "node_modules/.pnpm/set-co
 		}
 		return cookiesStrings;
 	}
-	module.exports = parse$1;
-	module.exports.parse = parse$1;
+	module.exports = parse;
+	module.exports.parse = parse;
 	module.exports.parseString = parseString;
 	module.exports.splitCookiesString = splitCookiesString;
-}) });
+}));
 
 //#endregion
 //#region main.ts
